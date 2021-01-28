02ed1d5822f6a14887a4824c20fcef3ba43a20ea
Aman Rai <raiaman15@gmail.com>
2021-01-28 07:38:51 +0530

    commit

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CompanyPageView, ProductPageView, DashboardView
 
 urlpatterns = [
+    path('', ProductPageView.as_view(), name='product_page'),
     path('company/', CompanyPageView.as_view(), name='company_page'),
-    path('product/', ProductPageView.as_view(), name='product_page'),
     path('dashboard/', DashboardView.as_view(), name='dashboard'),
 ]
