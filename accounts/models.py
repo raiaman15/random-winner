39cfd16eb4f220130372789d5cd8f262bcb439e0
Aman Rai <raiaman15@gmail.com>
2021-01-30 03:33:37 +0530

    commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+import pyotp
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
@@ -29,6 +30,7 @@ class CustomUser(AbstractUser):
     contact_number = models.CharField(
         max_length=17, blank=True, help_text='Your valid mobile number for OTP verification.'
     )
+    contact_secret = models.CharField(max_length=16, blank=True)
     contact_verified = models.BooleanField(default=False)
     is_willing_master = models.BooleanField(default=False)
     is_verified_master = models.BooleanField(default=False)
@@ -36,6 +38,11 @@ class CustomUser(AbstractUser):
         default=0.00, max_digits=7, decimal_places=2)
     investment_amount = models.DecimalField(
         default=0.00, max_digits=7, decimal_places=2)
+
+    def generate_otp(self):
+        self.contact_secret = pyotp.random_base32()
+        totp = pyotp.TOTP(self.contact_secret, interval=60)
+        PhoneNumberOTP(contact_number=self.contact_number, otp=totp).save()
 
     def apply_for_master(self):
         if self.identity_verified and self.contact_verified:
@@ -52,8 +59,12 @@ class CustomUser(AbstractUser):
 
 class PhoneNumberOTP(models.Model):
     contact_number = models.CharField(
-        max_length=17, blank=False, unique=True
+        max_length=17, blank=False, unique=True, editable=False
     )
+    otp = models.CharField(
+        max_length=6, blank=False, editable=False
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
 class BalanceTransaction(models.Model):
