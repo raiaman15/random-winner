93b94c0cf650b79ba37834ac46fab98a8e84540f
Aman Rai <raiaman15@gmail.com>
2021-01-29 17:49:31 +0530

    commit

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
-from config.validators import phone_regex
 
 
 class CustomUser(AbstractUser):
--- config/validators.py
+++ config/validators.py
@@ -1,5 +1,4 @@
 from django.core.exceptions import ValidationError
-from django.core.validators import RegexValidator
 from django.utils.translation import gettext_lazy as _
 
 
